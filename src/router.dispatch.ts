80ef385ebf617e28932670555c5470768cea8f93
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2023-12-18 14:29:12 +0000

    AOAF-261 add make file

--- src/router.dispatch.ts
+++ src/router.dispatch.ts
@@ -1,13 +1,18 @@
 // Do Router dispatch here, i.e. map incoming routes to appropriate router
 import { Application, Router } from "express";
-import indexRouter from "./routers/indexRouter";
+import { HomeRouter, HealthCheckRouter } from "./routers";
+import { healthcheckUrl, servicePathPrefix } from "./utils/constants/urls";
 import { errorHandler, pageNotFound } from "./routers/handlers/errors";
 
 const routerDispatch = (app: Application) => {
 
     const router = Router();
+    app.use(servicePathPrefix, router);
 
-    app.use("/", indexRouter);
+    router.use("/", HomeRouter);
+    router.use(healthcheckUrl, HealthCheckRouter);
+
+    app.use(errorHandler);
     app.use("*", pageNotFound);
 };
 
