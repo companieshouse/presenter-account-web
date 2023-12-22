eb68a916650f231fd416b9b47803b927bffc8a3c
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2023-12-22 08:59:40 +0000

    AOAF-261 terraform corrections

--- terraform/groups/ecs-service/data.tf
+++ terraform/groups/ecs-service/data.tf
@@ -34,7 +34,7 @@ data "aws_iam_role" "ecs_cluster_iam_role" {
 }
 
 data "aws_lb" "identity_lb" {
-  name = "${var.environment}-chs-internalapi"
+  name = "${var.environment}-chs-chgovuk"
 }
 
 data "aws_lb_listener" "identity_lb_listener" {
