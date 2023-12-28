6baf1b386cf7468c3c4c1738c4783c811cc8b1c7
Mohit Yadav <myadav@companieshouse.gov.uk>
2023-12-28 12:53:18 +0000

    login page of presenter account

--- src/routers/handlers/company/signin.ts
+++ src/routers/handlers/company/signin.ts
@@ -9,8 +9,7 @@ export class SignInHandler extends GenericHandler {
         this.viewData.title = "Sign in to Companies House";
         this.viewData.sampleKey = "login page";
     }
-    
-    // process request here and return data for the view
+
     public async execute (req: Request, response: Response, method: string = "GET"): Promise<any> {
         logger.info(`${method} request to sign in to companies house`);
         try {
@@ -27,12 +26,10 @@ export class SignInHandler extends GenericHandler {
         return this.viewData;
     }
 
-    // call service(s) to save data here
     private save (payload: any): Object {
         return Promise.resolve(true);
     }
 
-    // additional support method in handler
     private supportMethod2 (): Object {
         return Promise.resolve(true);
     }
