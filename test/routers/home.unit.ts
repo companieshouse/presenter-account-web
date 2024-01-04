bbfa88b08f295dc30b1884fd21e436dc1c24763c
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2024-01-04 15:49:08 +0000

    AOAF-261 homepage heading test

--- test/routers/home.unit.ts
+++ test/routers/home.unit.ts
@@ -3,11 +3,12 @@ import request from "supertest";
 import { servicePathPrefix } from "../../src/utils/constants/urls";
 
 describe("home page tests", () => {
+    const HEADING = "Apply for a Companies House presenter account for online filing (prototype)";
     it("should render the home page", async () => {
         const url = `${servicePathPrefix}`;
         const resp = await request(app).get(url);
 
         expect(resp.status).toBe(200);
-        expect(resp.text).toContain(``);
+        expect(resp.text).toContain(HEADING);
     });
 });
