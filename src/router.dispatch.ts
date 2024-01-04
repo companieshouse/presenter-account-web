61607827f75a71f521fd5fe11ba65c30a1b2edff
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2024-01-04 15:00:32 +0000

    AOAF-261 resolving and cleaning up

--- src/router.dispatch.ts
+++ src/router.dispatch.ts
@@ -1,11 +1,10 @@
 // Do Router dispatch here, i.e. map incoming routes to appropriate router
 import { Application, Router } from "express";
 import { HomeRouter, HealthCheckRouter } from "./routers";
-import { COMPANY_AUTH_PROTECTED_BASE, healthcheckUrl, servicePathPrefix } from "./utils/constants/urls";
+import { healthcheckUrl, servicePathPrefix } from "./utils/constants/urls";
 import { errorHandler, pageNotFound } from "./routers/handlers/errors";
 import { sessionMiddleware } from "./middleware/session.middleware";
 import { authenticationMiddleware } from "./middleware/authentication.middleware";
-import { companyAuthenticationMiddleware } from "./middleware/company.authentication.middleware";
 import { commonTemplateVariablesMiddleware } from "./middleware/common.variables.middleware";
 
 const routerDispatch = (app: Application) => {
@@ -21,7 +20,6 @@ const routerDispatch = (app: Application) => {
     // ------------- Enable login redirect -----------------
     const userAuthRegex = /^\/.+/;
     router.use(userAuthRegex, authenticationMiddleware);
-    router.use(`${COMPANY_AUTH_PROTECTED_BASE}`, companyAuthenticationMiddleware);
 
     app.use(commonTemplateVariablesMiddleware);
     app.use(errorHandler);
