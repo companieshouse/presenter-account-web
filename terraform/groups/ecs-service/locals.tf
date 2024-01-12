3e3e3f5d3842c692b442c0f5a0c3747729f6b66c
okoye-companieshouse <121234308+okoye-companieshouse@users.noreply.github.com>
2024-01-12 10:58:42 +0000

    Feature/aoaf 261 fe create node app including ecs terraform for the presenter account web service (#18)
    * AOAF-261 amend ALB/TG path
    
    * AOAF-261 Vault file fix (#14)
    
    * Feature/aoaf 261 fe create node app including ecs terraform for the presenter account web service (#15)
    
    * AOAF-261 Vault file fix
    
    * AOAF-261 Docker ignore fix
    
    * AOAF-261 Docker ignore add white space
    
    * Feature/aoaf 261 fe create node app including ecs terraform for the presenter account web service (#16)
    
    * AOAF-261 Vault file fix
    
    * AOAF-261 Docker ignore fix
    
    * AOAF-261 Docker ignore add white space
    
    * AOAF-261 modified access to docker_start script
    
    * AOAF-261 amend ALB/TG path

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -7,8 +7,8 @@ locals {
   container_port             = "3000" # default port required here until prod docker container is built allowing port change via env var
   docker_repo                = "presenter-account-web"
   lb_listener_rule_priority  = 12
-  lb_listener_paths          = ["/presenter-account-web/*"]
-  healthcheck_path           = "/presenter-account-web/healthcheck" #healthcheck path for presenter-account-web
+  lb_listener_paths          = ["/presenter-account/*"]
+  healthcheck_path           = "/presenter-account/healthcheck" #healthcheck path for presenter-account-web
   healthcheck_matcher        = "200"
   application_subnet_ids     = data.aws_subnets.application.ids
   kms_alias                  = "alias/${var.aws_profile}/environment-services-kms"
