042b4d2a0a35e5174bd4416ae2d4ad3673ce1e63
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2024-01-18 09:50:02 +0000

    Fixed typos

--- test/routers/handlers/you_cannot_use_this_service/you.cannon.use.ths.servce.unit.ts
+++ test/routers/handlers/you_cannot_use_this_service/you.cannon.use.ths.servce.unit.ts
@@ -4,7 +4,7 @@ import { PrefixedUrls } from "../../../../src/constants";
 
 describe("you cannot use this service page tests", () => {
     it("should render the you canot use this service page", async () => {
-        const resp = await request(app).get(PrefixedUrls.YOU_CANNOT_USE_THIS_SERVOCE);
+        const resp = await request(app).get(PrefixedUrls.YOU_CANNOT_USE_THIS_SERVICE);
 
         expect(resp.status).toBe(200);
         expect(resp.text).toContain("You need to complete an application form");
