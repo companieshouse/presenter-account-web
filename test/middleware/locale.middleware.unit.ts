cd1bb28584a2b1ee44e9d3b643604092abd26d7b
wdhda-ch <awoodhead@companieshouse.gov.uk>
2024-06-18 13:44:39 +0100

    fix lint

--- test/middleware/locale.middleware.unit.ts
+++ test/middleware/locale.middleware.unit.ts
@@ -13,51 +13,51 @@ const EN_NEW_SERVICE_TEXT = "This is a new service";
 const CY_NEW_SERVICE_TEXT = "Mae hwn y wasanaeth newydd";
 
 describe("Locale middleware test", () => {
-    
-    
+
+
     test("If lang is not set to either en or cy. Default to English", async () => {
         session.setExtraData(
             PRESENTER_ACCOUNT_SESSION_KEY,
             examplePresenterAccountDetails
-        );    
+        );
         const resp = await request(app).get(PrefixedUrls.ENTER_YOUR_DETAILS);
 
-            expect(resp.text).toContain(EN_NEW_SERVICE_TEXT)
-            expect(resp.status).toBe(200);
-        });
+        expect(resp.text).toContain(EN_NEW_SERVICE_TEXT);
+        expect(resp.status).toBe(200);
+    });
 
     test("If ?lang set to either en. Return English", async () => {
         session.setExtraData(
             PRESENTER_ACCOUNT_SESSION_KEY,
             examplePresenterAccountDetails
-        );    
-        
-        const resp = await request(app).get(PrefixedUrls.ENTER_YOUR_DETAILS+CHOICE_EN);
+        );
 
-            expect(resp.text).toContain(EN_NEW_SERVICE_TEXT)
-            expect(resp.status).toBe(200);
-        });
+        const resp = await request(app).get(PrefixedUrls.ENTER_YOUR_DETAILS + CHOICE_EN);
+
+        expect(resp.text).toContain(EN_NEW_SERVICE_TEXT);
+        expect(resp.status).toBe(200);
+    });
 
     test("If ?lang set to either cy. Return Welsh", async () => {
         session.setExtraData(
             PRESENTER_ACCOUNT_SESSION_KEY,
             examplePresenterAccountDetails
-        );    
-        
-        const resp = await request(app).get(PrefixedUrls.ENTER_YOUR_DETAILS+CHOICE_CY);
+        );
 
-            expect(resp.text).toContain(CY_NEW_SERVICE_TEXT)
-            expect(resp.status).toBe(200);
-        });
+        const resp = await request(app).get(PrefixedUrls.ENTER_YOUR_DETAILS + CHOICE_CY);
 
-        test("If &lang set to either cy. Return Welsh", async () => {
-            session.setExtraData(
-                PRESENTER_ACCOUNT_SESSION_KEY,
-                examplePresenterAccountDetails
-            );    
-            const resp = await request(app).get(PrefixedUrls.ENTER_YOUR_DETAILS+CHOICE_CY_ALSO);
-    
-                expect(resp.text).toContain(CY_NEW_SERVICE_TEXT)
-                expect(resp.status).toBe(200);
-            });
-    })
+        expect(resp.text).toContain(CY_NEW_SERVICE_TEXT);
+        expect(resp.status).toBe(200);
+    });
+
+    test("If &lang set to either cy. Return Welsh", async () => {
+        session.setExtraData(
+            PRESENTER_ACCOUNT_SESSION_KEY,
+            examplePresenterAccountDetails
+        );
+        const resp = await request(app).get(PrefixedUrls.ENTER_YOUR_DETAILS + CHOICE_CY_ALSO);
+
+        expect(resp.text).toContain(CY_NEW_SERVICE_TEXT);
+        expect(resp.status).toBe(200);
+    });
+});
