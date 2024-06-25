347a503bce89375e22187f38f99f0155f39c42ef
HarryBosworth <68226305+HarryBosworth@users.noreply.github.com>
2024-06-25 09:58:36 +0100

    Fixed typo

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ njk.addGlobal("ExternalUrls", ExternalUrls);
 njk.addGlobal("Urls", Urls);
 njk.addGlobal("servicePathPrefix", servicePathPrefix);
 njk.addGlobal("PrefixedUrls", PrefixedUrls);
-njk.addGlobal("SERVICE_NAME", 'presnter-account-web');
+njk.addGlobal("SERVICE_NAME", 'presenter-account-web');
 njk.addGlobal("PIWIK_URL", env.PIWIK_URL);
 njk.addGlobal("PIWIK_SITE_ID", env.PIWIK_SITE_ID);
 
