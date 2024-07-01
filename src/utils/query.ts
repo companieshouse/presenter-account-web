fca55aa0d3d72758734b71cdb042c2a968f3addd
wdhda-ch <awoodhead@companieshouse.gov.uk>
2024-07-01 14:15:39 +0100

    shorten regex - removing capture group

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -1,7 +1,7 @@
 
 const LANG_PARAM = new RegExp(/[?|&]lang=[a-zA-Z]{2}/);
 const END_OF_LINE_LANG_PARAM = new RegExp(LANG_PARAM.source + "$");
-const NON_END_OF_LINE_LANG_PARAM = /(?<JOINER>[?|&])lang=[a-zA-Z]{2}[?|&]/;
+const NON_END_OF_LINE_LANG_PARAM = /lang=[a-zA-Z]{2}[?|&]/;
 
 export function removeLangFromUrl(url: string): string {
     const lowerCaseUrl = url.toLocaleLowerCase();
@@ -14,5 +14,5 @@ export function removeLangFromUrl(url: string): string {
         return lowerCaseUrl.replace(END_OF_LINE_LANG_PARAM, "");
     }
     // lang query not at the end of the url
-    return lowerCaseUrl.replace(NON_END_OF_LINE_LANG_PARAM, "$<JOINER>");
+    return lowerCaseUrl.replace(NON_END_OF_LINE_LANG_PARAM, "");
 }
