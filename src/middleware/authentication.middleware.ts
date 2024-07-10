45086271cba1b75c5fd92bbcacafe37087365080
ch-skathirvel <136324774+ch-skathirvel@users.noreply.github.com>
2024-07-10 11:35:20 +0100

    AOAF-422 (#108)
    * AOAF-422
    Desc : Update redirect url with home url once the presenter account submitted.

--- src/middleware/authentication.middleware.ts
+++ src/middleware/authentication.middleware.ts
@@ -2,13 +2,23 @@ import { NextFunction, Request, Response } from "express";
 import { authMiddleware, AuthOptions } from "@companieshouse/web-security-node";
 import { env } from "../config";
 import { removeLangFromUrl } from "../utils/query";
-
+import { Urls, PrefixedUrls } from "../constants";
 export const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const CHS_URL = env.CHS_URL;
+    const returnUrl = getReturnCallbackURL(req);
     const authMiddlewareConfig: AuthOptions = {
         chsWebUrl: CHS_URL,
-        returnUrl: removeLangFromUrl(req.originalUrl)
+        returnUrl: removeLangFromUrl(returnUrl)
     };
 
     return authMiddleware(authMiddlewareConfig)(req, res, next);
 };
+
+function getReturnCallbackURL(req: Request){
+    if (req.originalUrl.endsWith(Urls.CONFIRMATION)){
+    // Returns the home URL, since the journey is completed
+        return PrefixedUrls.HOME;
+    } else {
+        return req.originalUrl;
+    }
+}
