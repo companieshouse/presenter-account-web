f949befd579357ee603b02d405e3d1c9d4a48347
ch-skathirvel <skathirvel@companieshouse.gov.uk>
2024-07-11 17:14:37 +0100

    JIRA: AOAF-424 Description: Added language parameter in the redirect URL and removed the unused logic from project.

--- src/middleware/authentication.middleware.ts
+++ src/middleware/authentication.middleware.ts
@@ -1,14 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { authMiddleware, AuthOptions } from "@companieshouse/web-security-node";
 import { env } from "../config";
-import { removeLangFromUrl } from "../utils/query";
 import { Urls, PrefixedUrls } from "../constants";
 export const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const CHS_URL = env.CHS_URL;
-    const returnUrl = getReturnCallbackURL(req);
     const authMiddlewareConfig: AuthOptions = {
         chsWebUrl: CHS_URL,
-        returnUrl: removeLangFromUrl(returnUrl)
+        returnUrl: getReturnCallbackURL(req)
     };
 
     return authMiddleware(authMiddlewareConfig)(req, res, next);
