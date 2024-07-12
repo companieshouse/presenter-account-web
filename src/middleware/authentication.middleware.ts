3c7e6727acdd590d354e238fe9d2035938ff1aa3
ch-skathirvel <136324774+ch-skathirvel@users.noreply.github.com>
2024-07-12 14:50:09 +0100

    JIRA: AOAF-424 (#110)
    * JIRA: AOAF-424
    Description: Added language parameter in the redirect url.
    
    * JIRA: AOAF-424
    Description: Addressed code review comments.

--- src/middleware/authentication.middleware.ts
+++ src/middleware/authentication.middleware.ts
@@ -1,21 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import { authMiddleware, AuthOptions } from "@companieshouse/web-security-node";
 import { env } from "../config";
-import { removeLangFromUrl } from "../utils/query";
 import { Urls, PrefixedUrls } from "../constants";
 export const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const CHS_URL = env.CHS_URL;
-    const returnUrl = getReturnCallbackURL(req);
     const authMiddlewareConfig: AuthOptions = {
         chsWebUrl: CHS_URL,
-        returnUrl: removeLangFromUrl(returnUrl)
+        returnUrl: getReturnCallbackURL(req)
     };
 
     return authMiddleware(authMiddlewareConfig)(req, res, next);
 };
 
 function getReturnCallbackURL(req: Request){
-    if (req.originalUrl.endsWith(Urls.CONFIRMATION)){
+    if (req.baseUrl.endsWith(Urls.CONFIRMATION)){
     // Returns the home URL, since the journey is completed
         return PrefixedUrls.HOME;
     } else {
