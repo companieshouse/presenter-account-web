b4177e124430ddc34ad0bc3fe2d10c5fc7192875
wdhda-ch <127737882+wdhda-ch@users.noreply.github.com>
2024-08-19 11:05:23 +0100

    Add the positive version of the convert test

--- test/utils/convert.details.unit.ts
+++ test/utils/convert.details.unit.ts
@@ -1,8 +1,15 @@
 import { convertSessionDetailsToApiDetails } from "../../src/utils/convert.details";
 import { PresenterSessionDetails } from "../../src/utils/session";
+import { examplePresenterAccountDetails } from "../mocks/example.presenter.account.details.mock";
 
 
 describe("convert details from session to api", () => {
+
+    test("if session details is a valid api details", () => {
+        const sessionDetails = {...examplePresenterAccountDetails, isBusinessRequired: true
+        } as unknown as PresenterSessionDetails;
+        expect(convertSessionDetailsToApiDetails(sessionDetails)).toStrictEqual(examplePresenterAccountDetails);
+    })
 
     test("if session details is not a valid api details", () => {
         const sessionDetails = {
