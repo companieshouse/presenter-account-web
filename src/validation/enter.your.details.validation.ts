042894460a1a5281d2570698c076d86a2f095369
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2024-08-27 10:44:23 +0100

    AOAF-476 Refactor validator

--- src/validation/enter.your.details.validation.ts
+++ src/validation/enter.your.details.validation.ts
@@ -20,55 +20,87 @@ export const formValidation = (req: Request) => {
     };
 
     return [
-        body("premises")
-            .not().isEmpty({ ignore_whitespace: true })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.PREMISES_BLANK", req))
-            .isLength({ max: CHARACTER_LENGTH.premises })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.PREMISES_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.premises}`))
-            .matches(character_validator)
-            .withMessage(getLocalesField("enter_your_details_validation_errors.PREMISES_INVALID_CHARACTER", req)),
-        body("addressLine1")
-            .not().isEmpty({ ignore_whitespace: true })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.ADDRESS_LINE_1_BLANK", req))
-            .isLength({ max: CHARACTER_LENGTH.addressLine1 })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.ADDRESS_LINE_1_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.addressLine1}`))
-            .matches(character_validator)
-            .withMessage(getLocalesField("enter_your_details_validation_errors.ADDRESS_LINE_1_INVALID_CHARACTER", req)),
-        body("addressLine2")
-            .isLength({ max: CHARACTER_LENGTH.addressLine2 })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.ADDRESS_LINE_2_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.addressLine2}`))
-            .matches(character_validator)
-            .withMessage(getLocalesField("enter_your_details_validation_errors.ADDRESS_LINE_2_INVALID_CHARACTER", req)),
-        body("townOrCity")
-            .not().isEmpty({ ignore_whitespace: true })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.TOWN_OR_CITY_BLANK", req))
-            .isLength({ max: CHARACTER_LENGTH.townOrCity })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.TOWN_OR_CITY_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.townOrCity}`))
-            .matches(character_validator)
-            .withMessage(getLocalesField("enter_your_details_validation_errors.TOWN_OR_CITY_INVALID_CHARACTER", req)),
-        body("postCode")
-            .not().isEmpty({ ignore_whitespace: true })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.POST_CODE_BLANK", req))
-            .isLength({ max: CHARACTER_LENGTH.postCode })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.POST_CODE_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.postCode}`))
-            .matches(character_validator)
-            .withMessage(getLocalesField("enter_your_details_validation_errors.POST_CODE_INVALID_CHARACTER", req)),
-        body("country")
-            .not().isIn(["choose"])
-            .withMessage(getLocalesField("enter_your_details_validation_errors.COUNTRY_BLANK", req)),
-        body("forename")
-            .not().isEmpty({ ignore_whitespace: true })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.FIRST_NAME_BLANK", req))
-            .isLength({ max: CHARACTER_LENGTH.forename })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.FIRST_NAME_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.forename}`))
-            .matches(character_validator)
-            .withMessage(getLocalesField("enter_your_details_validation_errors.FIRST_NAME_INVALID_CHARACTER", req)),
-        body("surname")
-            .not().isEmpty({ ignore_whitespace: true })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.LAST_NAME_BLANK", req))
-            .isLength({ max: CHARACTER_LENGTH.surname })
-            .withMessage(getLocalesField("enter_your_details_validation_errors.LAST_NAME_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.surname}`))
-            .matches(character_validator)
-            .withMessage(getLocalesField("enter_your_details_validation_errors.LAST_NAME_INVALID_CHARACTER", req)),
+        premiseValidation(req, CHARACTER_LENGTH),
+        addressLine1Validation(req, CHARACTER_LENGTH),
+        addressLine2Validation(req, CHARACTER_LENGTH),
+        townOrCityValidation(req, CHARACTER_LENGTH),
+        postCodeValidation(req, CHARACTER_LENGTH),
+        countryValidation(req),
+        forenameValidation(req, CHARACTER_LENGTH),
+        surnameValidation(req, CHARACTER_LENGTH),
     ];
 };
+
+function premiseValidation(req: Request, CHARACTER_LENGTH: {[key: string]: number}) {
+    return body("premises")
+        .not().isEmpty({ ignore_whitespace: true })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.PREMISES_BLANK", req))
+        .isLength({ max: CHARACTER_LENGTH.premises })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.PREMISES_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.premises}`))
+        .matches(character_validator)
+        .withMessage(getLocalesField("enter_your_details_validation_errors.PREMISES_INVALID_CHARACTER", req));
+}
+
+function addressLine1Validation(req: Request, CHARACTER_LENGTH: {[key: string]: number}) {
+    return body("addressLine1")
+        .not().isEmpty({ ignore_whitespace: true })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.ADDRESS_LINE_1_BLANK", req))
+        .isLength({ max: CHARACTER_LENGTH.addressLine1 })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.ADDRESS_LINE_1_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.addressLine1}`))
+        .matches(character_validator)
+        .withMessage(getLocalesField("enter_your_details_validation_errors.ADDRESS_LINE_1_INVALID_CHARACTER", req));
+}
+
+function addressLine2Validation(req: Request, CHARACTER_LENGTH: {[key: string]: number}) {
+    return body("addressLine2")
+        .isLength({ max: CHARACTER_LENGTH.addressLine2 })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.ADDRESS_LINE_2_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.addressLine2}`))
+        .matches(character_validator)
+        .withMessage(getLocalesField("enter_your_details_validation_errors.ADDRESS_LINE_2_INVALID_CHARACTER", req));
+}
+
+function townOrCityValidation(req: Request, CHARACTER_LENGTH: {[key: string]: number}) {
+    return body("townOrCity")
+        .not().isEmpty({ ignore_whitespace: true })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.TOWN_OR_CITY_BLANK", req))
+        .isLength({ max: CHARACTER_LENGTH.townOrCity })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.TOWN_OR_CITY_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.townOrCity}`))
+        .matches(character_validator)
+        .withMessage(getLocalesField("enter_your_details_validation_errors.TOWN_OR_CITY_INVALID_CHARACTER", req));
+}
+
+function postCodeValidation(req: Request, CHARACTER_LENGTH: {[key: string]: number}) {
+    return body("postCode")
+        .not().isEmpty({ ignore_whitespace: true })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.POST_CODE_BLANK", req))
+        .isLength({ max: CHARACTER_LENGTH.postCode })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.POST_CODE_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.postCode}`))
+        .matches(character_validator)
+        .withMessage(getLocalesField("enter_your_details_validation_errors.POST_CODE_INVALID_CHARACTER", req));
+}
+
+function countryValidation(req: Request) {
+    return body("country")
+        .not().isIn(["choose"])
+        .withMessage(getLocalesField("enter_your_details_validation_errors.COUNTRY_BLANK", req));
+}
+
+function forenameValidation(req: Request, CHARACTER_LENGTH: {[key: string]: number}) {
+    return body("forename")
+        .not().isEmpty({ ignore_whitespace: true })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.FIRST_NAME_BLANK", req))
+        .isLength({ max: CHARACTER_LENGTH.forename })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.FIRST_NAME_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.forename}`))
+        .matches(character_validator)
+        .withMessage(getLocalesField("enter_your_details_validation_errors.FIRST_NAME_INVALID_CHARACTER", req));
+}
+
+function surnameValidation(req: Request, CHARACTER_LENGTH: {[key: string]: number}) {
+    return body("surname")
+        .not().isEmpty({ ignore_whitespace: true })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.LAST_NAME_BLANK", req))
+        .isLength({ max: CHARACTER_LENGTH.surname })
+        .withMessage(getLocalesField("enter_your_details_validation_errors.LAST_NAME_LENGTH", req).replace("<length>", `${CHARACTER_LENGTH.surname}`))
+        .matches(character_validator)
+        .withMessage(getLocalesField("enter_your_details_validation_errors.LAST_NAME_INVALID_CHARACTER", req));
+}
