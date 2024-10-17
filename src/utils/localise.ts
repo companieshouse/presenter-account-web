4f69b3f4ce9bfeef2a4dc849112a89f620e97954
wdhda-ch <127737882+wdhda-ch@users.noreply.github.com>
2024-10-17 09:52:10 +0100

    WIP: Add in en and cy locales for status and months. update formatters and add new locale filter. Missing tests

--- src/utils/localise.ts
+++ src/utils/localise.ts
@@ -3,6 +3,7 @@ import { env } from "../config/index";
 import { i18nCh } from "@companieshouse/ch-node-utils";
 import { Request } from "express";
 import { LanguageCodes, QueryParameters } from "../constants";
+import { Environment } from "nunjucks";
 
 export const selectLang = (lang: any): string => {
     switch (lang) {
@@ -36,15 +37,23 @@ const localesSevice = LocalesService.getInstance(env.LOCALES_PATH, env.LOCALES_E
 export const getLocalesService = () => localesSevice;
 
 export function getLocalesField(fieldName: string, req: Request): string {
+    const language = getLanguageChoice(req);
+    return getLocalesFieldByLang(fieldName, language);
+}
+
+function getLocalesFieldByLang(fieldName: string, language: string): string {
     try {
-        const language = getLanguageChoice(req);
         const localesPath = localesSevice.localesFolder;
         const locales = i18nCh.getInstance(localesPath);
         return locales.resolveSingleKey(fieldName, language as string);
     } catch (e){
         throw new Error(`Unable to get locales file with ${fieldName}: ${e}`);
     }
+}
 
+export function getLocalesValue(fieldName: string, lang?: string): string {
+    const language = selectLang(lang)
+    return getLocalesFieldByLang(fieldName, language);
 }
 
 export function getLanguageChoice(req: Request): string {
@@ -52,4 +61,21 @@ export function getLanguageChoice(req: Request): string {
     const query_value = "true" === process.env.LOCALES_ENABLED ? req.query.lang : LanguageCodes.EN;
     const session_value = req.session?.getExtraData<string>(QueryParameters.LANG);
     return selectLang(query_value || session_value);
+}
+
+
+export function addLocale(env: Environment) {
+    for (const [name, localeTerm] of Object.entries(LocaleTerm)) {
+        env.addFilter(`Locale.${name}`, localeTerm);
+    }
+}
+
+
+const LocaleTerm = {
+    term: localeTerm,
+} as const;
+
+
+function localeTerm(term: string, language?: string): string {
+    return getLocalesValue(term.toLowerCase(), language);
 }
